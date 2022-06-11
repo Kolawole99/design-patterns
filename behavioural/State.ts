ea3cb51933ad05e4b3759b09135bae295ba7145c
Kolawole Ojo <philliphopeojo@gmail.com>
2022-06-11 19:08:43 +0100

    State pattern understood

---
+++ behavioural/State.ts
@@ -0,0 +1,104 @@
+/**
+ *
+ * State pattern is used to represent the state of the object in the context of the state machine pattern.
+ * State is a behavioral design pattern that lets an object alter its behavior when its internal state changes.
+ *
+ * It appears as if the object changed its class.
+ * The State pattern is closely related to the concept of a Finite-State Machine.
+ *
+ * The main idea is that, at any given moment, there’s a finite number of states which a program can be in.
+ * Within any unique state, the program behaves differently, and the program can be switched from one state to another instantaneously.
+ * However, depending on a current state, the program may or may not switch to certain other states.
+ * These switching rules, called transitions, are also finite and predetermined.
+ *
+ */
+
+/**
+ * The Context defines the interface of interest to clients. It also maintains a
+ * reference to an instance of a State subclass, which represents the current
+ * state of the Context.
+ */
+class Context {
+    /**
+     * @type {State} A reference to the current state of the Context.
+     */
+    private state: State;
+
+    constructor(state: State) {
+        this.transitionTo(state);
+    }
+
+    /**
+     * The Context allows changing the State object at runtime.
+     */
+    public transitionTo(state: State): void {
+        console.log(`Context: Transition to ${(<any>state).constructor.name}.`);
+        this.state = state;
+        this.state.setContext(this);
+    }
+
+    /**
+     * The Context delegates part of its behavior to the current State object.
+     */
+    public request1(): void {
+        this.state.handle1();
+    }
+
+    public request2(): void {
+        this.state.handle2();
+    }
+}
+
+/**
+ * The base State class declares methods that all Concrete State should
+ * implement and also provides a back reference to the Context object, associated
+ * with the State. This back reference can be used by States to transition the
+ * Context to another State.
+ */
+abstract class State {
+    protected context: Context;
+
+    public setContext(context: Context) {
+        this.context = context;
+    }
+
+    public abstract handle1(): void;
+
+    public abstract handle2(): void;
+}
+
+/**
+ * Concrete States implement various behaviors, associated with a state of the
+ * Context.
+ */
+class ConcreteStateA extends State {
+    public handle1(): void {
+        console.log('ConcreteStateA handles request1.');
+        console.log('ConcreteStateA wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateB());
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateA handles request2.');
+    }
+}
+
+class ConcreteStateB extends State {
+    public handle1(): void {
+        console.log('ConcreteStateB handles request1.');
+    }
+
+    public handle2(): void {
+        console.log('ConcreteStateB handles request2.');
+        console.log('ConcreteStateB wants to change the state of the context.');
+        this.context.transitionTo(new ConcreteStateA());
+    }
+}
+
+/**
+ * The client code.
+ */
+const context = new Context(new ConcreteStateA());
+context.request1();
+context.request2();
+
